548bb443c59148c247a9f2faeef1021a432bc21d
Егор Брон <bataikinegor@yandex.ru>
2021-08-10 23:20:58 +0300

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,12 +1,21 @@
 import discord, asyncio, aiohttp, os, sys, time, datetime, random, requests, aeval # Импорты библиотек
 from discord.ext import commands, tasks # Импорты дополнений (extensions) из d.py
+
 bot = commands.Bot(command_prefix = commands.when_mentioned_or('!'), intents = discord.Intents.all())
 # Пояснение: тут префикс - это либо упоминание бота, либо "!"; intents - "нмерения доступа к шлюзам API", они нужны для получения некоторых данных (я выбрал все, но можете посмотреть в документации)
-minify_text = lambda txt: f'{txt[:-900]}...\n# ...и ещё {len(txt.replace(txt[:-900], ""))} символов' if len(txt) >= 1024 else txt # Захотелось использовать лямбду и всё в одну строку...
+
+def minify_text(txt):
+    if len(txt) >= 1024:
+        return f'''{txt[:-900]}...
+        # ...и ещё {len(txt.replace(txt[:-900], ""))} символов...'''
+    else:
+        return txt # Захотелось использовать лямбду и всё в одну строку... но решил хоть как-то сделать читабельней
+
 @bot.command(aliases = ['eval', 'aeval', 'evaulate', 'выполнить', 'exec', 'execute'])
 async def __eval(ctx, *, content):
     if ctx.author.id not in bot.owner_ids: return await ctx.send("Кыш!") # Защита от os.system('format C') :)
-    code = "\n".join(content.split("\n")[1:])[:-3] if content.startswith("```") and content.endswith("```") else content # Проверка на то, записан ли код в Markdown'овском блоке кода и его "очистка"
+    # Проверка на то, записан ли код в Markdown'овском блоке кода и его "очистка":
+    code = "\n".join(content.split("\n")[1:])[:-3] if content.startswith("```") and content.endswith("```") else content
     standart_args = { # Стандартные библиотеки и переменные, которые будут определены в коде. Для удобства. Кстати, я уже добавил несколько встроенных либ и переменных из d.py
         "discord": discord,
         "commands": commands,
@@ -22,7 +31,7 @@ async def __eval(ctx, *, content):
         "random": random,
         "requests": requests
     }
-    start = time.time() # import time, для расчёта времени выполнения
+    start = time.time() # запись стартового таймстампа для расчёта времени выполнения
     try:
         r = await aeval.aeval(f"""{code}""", standart_args, {}) # выполняем код
         ended = time.time() - start # рассчитываем конец выполнения
@@ -32,7 +41,7 @@ async def __eval(ctx, *, content):
             """
              Есть нюанс: если входные/выходные данные будут длиннее 1024 символов, то эмбед не отправится, а функция выдаст ошибку.
              Именно поэтому сверху стоит print(r), а так же есть функция minify_text, которая
-             минифицирует текст для эмбеда во избежание БэдРеквеста (слишком много символов). Поставил специально лимит на 900, чтобы точно хватило
+             минифицирует текст для эмбеда во избежание БэдРеквеста (который тут возникает когда слишком много символов). Поставил специально лимит на 900, чтобы точно хватило
             """
             embed.add_field(name = f'Входные данные:', value = f'`{minify_text(code) }`')
             embed.add_field(name = f'Выходные данные:', value = f'`{minify_text(r) }`', inline=False) 
